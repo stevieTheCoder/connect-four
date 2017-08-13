fd0c1fe4366f0e5eb0867e81fa90ebd602aaf91d
Steve <steven.rogers1990@gmail.com>
2017-08-13 17:51:56 +0100

    names and colours for players can be set and accessed

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,13 +4,26 @@ module ConnectFour
 	RSpec.describe Player do
 
 		let(:blank) {Player.new({})}
-		let(:steve) {Player.new("steve", "RED")}
-		let(:rebecca) {Player.new("Rebecca", "YELLOW")}
+		let(:steve) {Player.new({ name: "steve", colour: "RED" })}
 
 		describe "#initialize" do
 			context "initialize with no input" do
 				it "raises an exception" do
 					expect{ blank }.to raise_error KeyError
+				end
+			end
+
+			context "can be initialized with a name and colour" do
+				it "accepts input hash" do
+					expect{ steve }.to_not raise_error
+				end
+			end
+		end
+
+		describe "#name" do
+			context "variables can be accessed" do
+				it "returns the name" do
+					expect(steve.name).to eq "steve"
 				end
 			end
 		end
