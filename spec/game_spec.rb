63f9227b5317a85804f43e7377beced621326156
Steve <steven.rogers1990@gmail.com>
2017-08-15 13:36:27 +0100

    switch player function working

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -18,5 +18,23 @@ module ConnectFour
         end
       end
     end
+
+    describe "#switch_players" do
+      context "switches players at the end of the turn" do
+        it "changes the current_player to the other_player" do
+          game = Game.new([steve, rebecca])
+          other_player = game.other_player
+          game.switch_players
+          expect(game.current_player).to eq other_player
+        end
+
+        it "changes the other_player to the current_player" do
+          game = Game.new([steve, rebecca])
+          current_player = game.current_player
+          game.switch_players
+          expect(game.other_player).to eq current_player
+        end
+      end
+    end
   end
 end
